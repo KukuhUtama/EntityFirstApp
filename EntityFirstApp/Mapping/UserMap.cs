4c7d1df94d635927542611a679cf023886f526b2
kukuhutama <kukuh2utama@gmail.com>
2015-10-14 23:39:04 +0700

    Fifth commit

---
+++ EntityFirstApp/Mapping/UserMap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFirstApp.Mapping
+{
+    public class ProfileMap : EntityTypeConfiguration<Profile>
+    {
+        public ProfileMap()
+        {
+            //key  
+            this.HasKey(t => t.ID);
+
+            //fields  
+            this.Property(t => t.FirstName);
+            this.Property(t => t.LastName);
+            this.Property(t => t.Address).HasMaxLength(100).HasColumnType("nvarchar");
+            this.Property(t => t.AddedDate);
+            this.Property(t => t.ModifiedDate);
+            this.Property(t => t.IP);
+
+            //table  
+            ToTable("UserProfiles");
+
+            //Relationship
+            this.HasRequired(t => t.User).WithRequiredDependent(u => u.Profile);
+        }
+    }
+}
