e54e025f1b84c6d798ada9475eea6a7682f04d7d
KukuhUtama <kukuh2utama@gmail.com>
2015-11-03 07:31:44 +0700

    Edit Context package

---
+++ EntityFirstApp/Context/IDatabaseFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFirstApp.Context
+{
+    public interface IDatabaseFactory<D> : IDisposable where D : DbContext
+    {
+        D Get();
+    }
+}
