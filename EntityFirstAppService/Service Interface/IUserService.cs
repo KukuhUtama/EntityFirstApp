78105c356a636b49c5f0d456ec24e5889ef5cb21
KukuhUtama <kukuh2utama@gmail.com>
2015-11-05 14:57:28 +0700

    Service seed

--- EntityFirstAppService/Service Interface/IUserService.cs
+++ EntityFirstAppService/Service Interface/IUserService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EntityFirstAppService
+namespace EntityFirstAppService.Service_Interface
 {
-    public class Class1
+    interface IUserService
     {
     }
 }
