f10ffd88fe58d581e081dd91aecd061de93db011
KukuhUtama <kukuh2utama@gmail.com>
2015-11-05 18:31:42 +0700

    Ninject seed

--- EntityFirstAppService/Service Interface/IUserService.cs
+++ EntityFirstAppService/Service Interface/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntityFirstApp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,51 @@ using System.Threading.Tasks;
 
 namespace EntityFirstAppService.Service_Interface
 {
-    interface IUserService
+    public interface IUserService
     {
+        /// <summary>
+        /// Return all User.
+        /// </summary>
+        /// <returns>All User.</returns>
+         IEnumerable<User> GetAll();
+
+        /// <summary>
+        /// Find the User by identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>Transport if found, null if the specified id is not found.</returns>
+         User GetUserById(Guid id);
+
+        /// <summary>
+        /// Add new Transport.
+        /// </summary>
+        /// <param name="item">The User parameter.</param>
+         void AddUser(User item);
+
+        /// <summary>
+        /// Delete the User.
+        /// </summary>
+        /// <param name="item">The entity.</param>
+         void DeleteUser(User item);
+
+        /// <summary>
+        /// Updates existing User.
+        /// </summary>
+        /// <param name="item">The User parameter.</param>
+         void UpdateUser(User item);
+
+        /// <summary>
+        /// Gets the transports by company identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>User if found, null if the specified CompanyId is not found.</returns>
+         User GetUserByCompanyId(Guid id);
+
+        /// <summary>
+        /// Gets the transports by code.
+        /// </summary>
+        /// <param name="code">The code identifier.</param>
+        /// <returns>User if found, null if the specified code is not found.</returns>
+         User GetUserByCode(string code);
     }
 }
--- EntityFirstAppService/Service/UserService.cs
+++ EntityFirstAppService/Service/UserService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EntityFirstApp;
+using EntityFirstApp.Repository_Interface;
+using EntityFirstAppService.Service_Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,79 @@ using System.Threading.Tasks;
 
 namespace EntityFirstAppService.Service
 {
-    class UserService
+    public class UserService : IUserService
     {
+
+        private readonly IUserRepository userRepository;
+
+        public UserService(IUserRepository _userRepository)
+        {
+            this.userRepository = _userRepository;
+        }
+        /// <summary>
+        /// Return all User.
+        /// </summary>
+        /// <returns>All User.</returns>
+        IEnumerable<User> GetAll()
+        {
+            return userRepository.GetAll();
+        }
+
+        /// <summary>
+        /// Find the User by identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>Transport if found, null if the specified id is not found.</returns>
+        User GetUserById(Guid id)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Add new Transport.
+        /// </summary>
+        /// <param name="item">The User parameter.</param>
+        void AddUser(User item)
+        {
+
+        }
+
+        /// <summary>
+        /// Delete the User.
+        /// </summary>
+        /// <param name="item">The entity.</param>
+        void DeleteUser(User item)
+        {
+
+        }
+
+        /// <summary>
+        /// Updates existing User.
+        /// </summary>
+        /// <param name="item">The User parameter.</param>
+        void UpdateUser(User item)
+        {
+
+        }
+
+        /// <summary>
+        /// Gets the transports by company identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>User if found, null if the specified CompanyId is not found.</returns>
+        User GetUserByCompanyId(Guid id)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the transports by code.
+        /// </summary>
+        /// <param name="code">The code identifier.</param>
+        /// <returns>User if found, null if the specified code is not found.</returns>
+        User GetUserByCode(string code)
+        {
+            return null;
+        }
     }
 }
